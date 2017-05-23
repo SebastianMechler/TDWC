e50f3e305c67b1308570472043e465bfd60de7fa
Anna <atpi@outlook.de>
2017-05-23 16:41:27 +0200

    - Implemented Drawer and Desk interaction

---
+++ Source/TheDevilInWhiteCity/Interactable_Drawer.cpp
@@ -0,0 +1,89 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TheDevilInWhiteCity.h"
+#include "Interactable_Drawer.h"
+
+
+// Sets default values
+AInteractable_Drawer::AInteractable_Drawer()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AInteractable_Drawer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AInteractable_Drawer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if(!interacted)
+	{
+		return;
+	}
+
+	// 0.016f
+	lerpTime += DeltaTime * openSpeed;
+
+	// 0.0f - 1.0f
+	FVector newPosition = FMath::Lerp(startVector, endVector, lerpTime);
+
+	SetActorLocation(newPosition);
+
+	if(lerpTime > 1)
+	{
+		interacted = false;
+		endVector = FVector(0.0f, 0.0f, 0.0f);
+	}
+}
+
+void AInteractable_Drawer::Interact(AActor * a_player)
+{
+
+	auto head = this->GetAttachParentActor();
+
+	if(head == nullptr)
+	{
+		return;
+	}
+
+	if (endVector != FVector::ZeroVector)
+	{
+		return;
+	}
+
+	FVector headFowardVector = head->GetActorRightVector();
+	startVector = this->GetActorLocation();
+
+	if(!isOpen)
+	{
+		endVector = startVector + headFowardVector * openDistance;
+		isOpen = true;
+	}
+	else
+	{
+		endVector = startVector - headFowardVector * openDistance;
+		isOpen = false;
+	}
+
+	lerpTime = 0.0f;
+	interacted = true; 
+
+
+	
+}
+
+EInteractionType AInteractable_Drawer::GetInteractionType()
+{
+	return EInteractionType::World;
+}
+void AInteractable_Drawer::OnViewSpace(AActor* a_player)
+{
+
+}
---
+++ Source/TheDevilInWhiteCity/Interactable_Drawer.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Interface_Interactable.h"
+#include "Interactable_Drawer.generated.h"
+
+UCLASS()
+class THEDEVILINWHITECITY_API AInteractable_Drawer : public AActor, public IInterface_Interactable
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AInteractable_Drawer();
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	FVector startVector = FVector(0.0f, 0.0f, 0.0f);
+	FVector endVector = FVector(0.0f, 0.0f, 0.0f);
+	float lerpTime = 0.0f;
+	bool interacted = false; 
+	bool isOpen = false; 
+	
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay")
+	float openDistance = 40.f;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay")
+	float openSpeed = 2.f;
+
+	void Interact(AActor* a_player) override;
+	EInteractionType GetInteractionType() override;
+	void OnViewSpace(AActor* a_player) override;
+	
+};
